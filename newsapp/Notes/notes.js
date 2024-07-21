b5930e6f72beee827cc41b4b722fc5cacd985b4b
Areeba Amjad <areeba.amjad2020@gmail.com>
2024-07-21 06:13:13 -0700

    Commit 01: Component Structure of our GlobalNewsDigest React App

---
+++ newsapp/Notes/notes.js
@@ -0,0 +1,11 @@
+
+// ========================================= 01 ========================================
+/*
+    Components are not functions in React but they can be classes also.
+    App.js is entry point and index.js import App.js and App ko index.html main dalta he
+    create-react-app by default prefer function based components
+    use rcc for class based components
+    Diff between class and Func baseed components
+    1. In class based we return it using the render function and in function we directly return 
+
+*/
---
+++ newsapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello my first class based components
+      </div>
+    )
+  }
+}
